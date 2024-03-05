c6453435991c7d703c94c4f942e47ce8f6752fa8
TOMLEUNGKS <lks1707@connect.hku.hk>
2024-03-05 14:30:54 -0500

    update about me

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,6 +1,11 @@
 import AboutMe from "../AboutMe";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
 import HeroSection from "../HeroSection";
+import MyPortfolio from "../MyPortfolio";
 import MySkills from "../MySkills";
+import Testimonial from "../Testimonials";
+
 
 export default function Home() {
     return (
@@ -8,6 +13,10 @@ export default function Home() {
         <HeroSection/>
         <MySkills/>
         <AboutMe />
+        <MyPortfolio/>
+        <Testimonial/>
+        <ContactMe/>
+        <Footer />
         </>
-    )
+    );
 }
