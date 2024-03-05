26617b008a42c498ec4d1173ae64296e76dcc6ac
TOMLEUNGKS <lks1707@connect.hku.hk>
2024-03-04 22:25:37 -0500

    update myskills and heroSection

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,9 @@
+export default function AboutMe(){
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/tom_linkedin_color-removebg.png" alt="About Me" />
+            </div>
+        </section>
+    )
+}
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,21 +3,20 @@ export default function HeroSection() {
         <section id="heroSection" className="hero--section">
             <div className="hero--section--content--box">
                 <div className="hero--section--content">
-                    <p className="section--title">Hello, I am Tom</p>
+                    <p className="section--title">Hello, welcome!</p>
                     <h1 className="hero--section--title">
-                        <span className="hero--section--title--color">Software Development</span>{""}
+                        <span className="hero--section--title--color">I am Tom.</span>{""}
                         <br />
-                        QA Automation
+                        A Software Engineer.
                     </h1>
                     <p className="hero--section--description">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                        <br />  Quidem excepturi culpa quasi
+                        This website is built using React JS. 
                     </p>
                 </div>
                 <button className="btn btn-primary">Get In Touch</button>
             </div>
             <div className="hero--section--img">
-                <img src="./img/tom_linkedin_color-removebg.png" alt="Hero Section" />
+                <img src="./img/coding_bg.png" alt="Hero Section" />
             </div>
         </section>
     )
