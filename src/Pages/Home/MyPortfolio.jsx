decfd21e9dd58e7fea5552683a8b2afeb98fb115
TOMLEUNGKS <lks1707@connect.hku.hk>
2024-03-06 16:07:13 -0500

    update portfolio content

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -36,9 +36,9 @@ export default function MyPortfolio() {
             <div className="portfolio--section--card--content">
               <div>
                 <h3 className="portfolio--section--title">{item.title}</h3>
-                <p className="text-md">{item.description}</p>
+                <p className="text-md" style={{paddingTop: "8px"}}>{item.description}</p>
               </div>
-              <p className="text-sm portfolio--link">
+              <a className="text-sm portfolio--link" href={item.url} rel="noreferrer" target="_blank">
                 {item.link}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -55,7 +55,7 @@ export default function MyPortfolio() {
                     stroke-linejoin="round"
                   />
                 </svg>
-              </p>
+              </a>
             </div>
           </div>
         ))}
