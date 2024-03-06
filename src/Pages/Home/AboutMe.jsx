290dfa37b5761a0a7b8cfd2e414f879b5943be36
TOMLEUNGKS <lks1707@connect.hku.hk>
2024-03-06 16:07:25 -0500

    update about me content

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -12,9 +12,16 @@ export default function AboutMe(){
                         I love challenges, and I keep learning.
                     </p>
                     <p className="hero--section-description">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-                        officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-                        recusandae?
+                        To me, learning new things and solving problems bring me genuine joy.
+                    </p>
+                    <p className="hero--section-description">
+                        Since graduation, I have been working in the IT industry for 6 years, with a focus on software development and test automation.
+                    </p>
+                    <p className="hero--section-description">
+                        I completed a Master in Advanced Information Systems, where I studie a range of computer science topics.
+                    </p>
+                    <p className="hero--section-description">
+                        Currently, I am based in Toronto, Canada (where I tried snowboarding for the very first time!)
                     </p>
                 </div>
             </div>
